4343900e469d6df58f2fd169509a1db7415e12ea
SimoneBuccolieri <simobucc2nd@gmail.com>
2025-03-11 17:57:46 +0100

    wip

---
+++ src/components/Section.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="container mx-auto px-8 py-12 md:py-24 lg:py-32">
+    <slot></slot>
+  </section>
+</template>
+
+<script setup></script>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Roboto", "sans-serif"], // Imposta Roboto come font principale
+            },
+        },
+    },
+    plugins: [],
+};
